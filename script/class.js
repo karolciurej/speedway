88a96cd4def966f316ebd3bac2f565acbddb50f2
Karol <xdarkin1w@gmail.com>
2023-10-17 09:54:26 +0200

    fixing imports

--- script/class.js
+++ script/class.js
@@ -1,4 +1,4 @@
-class Speedway{
+export default class Speedway{
     width;
     height;
     ctx;
--- script/script.js
+++ script/script.js
@@ -1,1 +1,10 @@
-import './class';
+import Speedway from './class.js'
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+ctx.beginPath()
+ctx.ellipse(250, 200, 200, 150, 0, 0, 2 * Math.PI);
+ctx.fill()
+
+const speed = new Speedway(50, 50, ctx)
+console.log(speed)
