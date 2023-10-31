a164b679c1f943dbb3c1840a0cf8ec27611cafb2
Karol <xdarkin1w@gmail.com>
2023-10-31 09:30:36 +0100

    quick fix

--- script/motor.js
+++ script/motor.js
@@ -1,14 +1,24 @@
 export default class Motor {
-    constructor() {
+    pos = {x: 0, y: 0}
+    angle = 0
+    constructor(canvasId, width, height, count) {
         this.canvas = document.getElementById(canvasId);
         this.ctx = this.canvas.getContext("2d");
-        this.width = this.canvas.width;
-        this.height = this.canvas.height;
+        this.width = width
+        this.height = height
+        this.pos.x = this.canvas.width/2;
+        this.pos.y = 600 + 15 * count;
+
     }
-    drawMotor(){
+    drawMotor() {
+        this.ctx.save();
         this.ctx.fillStyle = "blue";
-        this.ctx.fillRect(0, 0, this.width, this.height);
-        this.ctx.beginPath();
-        
+        this.ctx.translate(this.pos.x + this.width / 2, this.pos.y + this.height / 2); 
+        this.ctx.rotate((this.angle * Math.PI) / 180); 
+        this.ctx.fillRect(-this.width / 2, -this.height / 2, this.width, this.height); 
+        this.ctx.restore();
     }
+    update() {
+        
+    }    
   }
--- script/script.js
+++ script/script.js
@@ -1,4 +1,8 @@
 import Speedway from './speedway.js';
+import Motor from './motor.js';
 
 const speedway = new Speedway("canvas");
 speedway.drawSpeedway();
+
+const motor1 = new Motor("canvas", 20,10,0)
+motor1.drawMotor()
